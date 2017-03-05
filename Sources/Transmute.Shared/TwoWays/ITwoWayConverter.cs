3c390177ec4481d9086c76ecdb31da9db4099787
Ned Lanon <ned.lanon@outlook.com>
2017-03-05 14:11:12 +0100

    Added TwoWayConverters.

---
+++ Sources/Transmute.Shared/TwoWays/ITwoWayConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Transmute
+{
+	public interface ITwoWayConverter<TSource,TTarget>
+	{
+		IConverter<TSource, TTarget> ToTarget { get; }
+
+		IConverter<TTarget, TSource> ToSource { get; }
+	}
+}
---
+++ Sources/Transmute.Shared/TwoWays/TwoWayConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace Transmute
+{
+	using System;
+
+	public class TwoWayConverter<TSource,TTarget> : ITwoWayConverter<TSource, TTarget>
+	{
+		public TwoWayConverter(Func<TSource, TTarget> toTarget, Func<TTarget, TSource> toSource) : this(new RelayConverter<TSource,TTarget>(toTarget), new RelayConverter<TTarget, TSource>(toSource))
+		{
+		}
+
+		public TwoWayConverter(IConverter<TSource, TTarget> toTarget, IConverter<TTarget, TSource> toSource)
+		{
+			this.ToTarget = toTarget;
+			this.ToSource = toSource;
+		}
+
+		public IConverter<TSource, TTarget> ToTarget { get; }
+
+		public IConverter<TTarget, TSource> ToSource { get; }
+	}
+}
